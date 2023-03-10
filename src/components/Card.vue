bd1fe7317fa7779ddd3851d21d3416e1d3f0b97a
cp-cp <wxymmyx@126.com>
2023-03-10 17:57:14 +0800

    实现回答的逐渐键入

---
+++ src/components/Card.vue
@@ -0,0 +1,37 @@
+<template>
+    <el-card class="text-card">
+      <div class="text-container">
+        {{ displayText }}
+      </div>
+    </el-card>
+  </template>
+  
+  <script>
+  export default {
+    props:['content'],
+    data() {
+      return {
+        text: this.content,
+        currentLength: 0
+      }
+    },
+    computed: {
+      displayText() {
+        return this.text.substring(0, this.currentLength)
+      }
+    },
+    mounted() {
+      this.timer = setInterval(() => {
+        if (this.currentLength < this.text.length) {
+          this.currentLength++
+        } else {
+          clearInterval(this.timer)
+        }
+      }, 100)
+    },
+    beforeDestroy() {
+      clearInterval(this.timer)
+    }
+  }
+  </script>
+  
--- src/components/MainTalk.vue
+++ src/components/MainTalk.vue
@@ -4,40 +4,47 @@
             什么都没有
         </el-header>
         <el-main class="chat-message">
-        <el-scrollbar wrap-class="chat-area">
-            <div class="chat-messages">
-            <div :key="index"  v-for="(card, index) in cards" >
-                <el-card v-if="card.title==='1'" style="width: 300px;height: auto;margin-left: 20px;"  shadow="hover">
-                <div  class="text item" style="">
-                    {{ card.body }}
+            <el-scrollbar wrap-class="chat-area">
+                <div class="chat-messages">
+                    <div :key="index" v-for="(card, index) in cards">
+                        <Card v-if="card.title === '1'" 
+                            style="width: 300px;height: auto;margin-left: 20px;"
+                            shadow="hover"
+                            :content="card.body">
+                            <!-- <div class="text item" style="">
+                                {{ card.body }}
+                            </div> -->
+                        </Card>
+                        <el-card v-if="card.title === '2'"
+                            style="width: 300px;height: auto;margin-left: auto;margin-right: 20px;margin-top: 20px;"
+                            shadow="hover">
+                            <div class="text item" style="">
+                                {{ card.body }}
+                            </div>
+                        </el-card>
+                    </div>
                 </div>
-                </el-card>                
-                <el-card v-if="card.title==='2'" style="width: 300px;height: auto;margin-left: auto;margin-right: 20px;margin-top: 20px;"  shadow="hover">
-                <div  class="text item" style="">
-                    {{ card.body }}
-                </div>
-                </el-card>
-            </div>
-            </div>
-        </el-scrollbar>
+            </el-scrollbar>
         </el-main>
         <el-footer class="chat-input">
-         <Input @my-event="onMyEvent" ref="sendInput"></Input>
-        </el-footer>        
+            <Input @my-event="onMyEvent" ref="sendInput"></Input>
+        </el-footer>
     </el-container>
 </template>
 
 <script>
 import Input from './Input.vue';
+import Card from './Card.vue';
 export default {
-    components: { 
+    components: {
         Input,
+        Card
     },
     data() {
         return {
-        isMainRendered: false, 
-        cards: [{ title: '1', body: '我能帮你什么?' }],
-        message:{message:""}
+            isMainRendered: false,
+            cards: [{ title: '1', body: '我能帮你什么?' }],
+            message: { message: "" }
         }
     },
     methods: {
@@ -45,34 +52,32 @@ export default {
             return new Promise(resolve => {
                 setTimeout(resolve, delay)
             })
-            },
+        },
         async onMyEvent(payload) {
             const button = this.$refs.sendInput.$refs.sendTo;
             button.disabled = true;
-            this.message=payload;
+            this.message = payload;
             console.log(payload);
             this.cards.push(
                 { title: '2', body: this.message.message }
             );
             await this.wait(500);
-            button.disabled=false;
+            button.disabled = false;
             this.cards.push(
-                {title:'1',body: "hi"}
-            )     
+                { title: '1', body: "hi" }
+            )
             const elMain = document.querySelector('.el-main');
             // 在下一次 DOM 更新循环结束后执行自动滚动的代码
             this.$nextTick(() => {
-            this.isMainRendered = true;
-            // 如果 el-main 组件存在，则自动滚动到最下面
-            if (elMain) {
-                elMain.scrollTop = elMain.scrollHeight;
-            }       
+                this.isMainRendered = true;
+                // 如果 el-main 组件存在，则自动滚动到最下面
+                if (elMain) {
+                    elMain.scrollTop = elMain.scrollHeight;
+                }
             })
-         },
+        },
     }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
