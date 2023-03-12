604c93d14b44de95d2c2de6da76610a9b8ac2564
cp-cp <wxymmyx@126.com>
2023-03-13 00:18:05 +0800

    完成问题表格和分页器

--- src/components/QuestionList.vue
+++ src/components/QuestionList.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
         <el-table
-        :data="tableData"
+        :data="tableData.slice((this.currentPage-1)*this.pageSize,this.currentPage*this.pageSize)"
         stripe
+        height="550px"
         border
         style="width: 100%;height: 100%;">
             <el-table-column type="expand">
@@ -33,6 +34,15 @@
                 </template>
             </el-table-column>
         </el-table>
+`       <el-pagination align='center' 
+        @size-change="handleSizeChange" 
+        @current-change="handleCurrentChange" 
+        :current-page="currentPage" 
+        :page-sizes="[1,5,10,20]" 
+        :page-size="pageSize" 
+        layout="total, sizes, prev, pager, next, jumper" 
+        :total="tableData.length">
+        </el-pagination>
     </div>
 </template>
 <script>
@@ -40,10 +50,23 @@
     export default {
         data() {
             return {
-                tableData: [{asker:"",content:"",id:""}]
+                tableData: [{asker:"",content:"",id:""}],
+                currentPage: 1, // 当前页码
+                pageSize: 8 // 每页的数据条数
             }
         },
         methods: {
+            //每页条数改变时触发 选择一页显示多少行
+            handleSizeChange(val) {
+                console.log(`每页 ${val} 条`);
+                this.currentPage = 1;
+                this.pageSize = val;
+            },
+            //当前页改变时触发 跳转其他页
+            handleCurrentChange(val) {
+                console.log(`当前页: ${val}`);
+                this.currentPage = val;
+            },
             resetDateFilter() {
                 this.$refs.filterTable.clearFilter('date');
             },
@@ -90,5 +113,9 @@
     margin-left: 30px;
     margin-bottom: 0;
     width: 50%;
+  }
+  .el-form-item{
+    margin-top: 18px;
+    margin-bottom: 18px;
   }
 </style>
