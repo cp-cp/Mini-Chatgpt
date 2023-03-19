0bc07589b6f983632092d148878f03f0c4448a45
cp-cp <wxymmyx@126.com>
2023-03-19 10:17:09 +0800

    实现图云

--- src/components/image.vue
+++ src/components/image.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-      <img :src="'http://localhost:8081/api/image?timestamp=' + timestamp" />
+      <img :src="'http://localhost:8080/api/image?timestamp=' + timestamp" style="max-width: 400px; max-height: 300px;"/>
     </div>
   </template>
   <script>
--- src/components/wordcloud.vue
+++ src/components/wordcloud.vue
@@ -1,83 +1,67 @@
 <template>
     <el-card style="height: 100%;">
-        <el-form ref="form" :model="form" label-width="80px">
-            <el-form-item label="输入文本" >
-                <el-input type="textarea" v-model="form.content"   :autosize="{ minRows: 5, maxRows: 10}"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-button @click="addAns">立即创建</el-button>
-                <el-button>取消</el-button>
-            </el-form-item>
-        </el-form>
-        <!-- <Images></Images> -->
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="输入文本">
+          <el-input type="textarea" v-model="form.content" :autosize="{ minRows: 5, maxRows: 10}" @input="discover"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="addText">立即创建</el-button>
+          <el-button @click="toggleImage">
+            {{ imag ? '隐藏图片' : '显示图片' }}
+        </el-button>
+        </el-form-item>
+      </el-form>
+      <Images v-if="imag"></Images>
     </el-card>
-</template>
-<script>
-import store from '@/store/store';
-import axios from 'axios';
-import Images from './image.vue';
-export default {
-  components: { Images },
-//    props: ['quest'],
+  </template>
+  
+  <script>
+  import store from "@/store/store";
+  import axios from "axios";
+  import Images from "./image.vue";
+  
+  export default {
+    components: { Images },
     data() {
-        return {
-            form: {
-                title: '',
-                content: ''
-            },
-            question: {
-                content:"",
-                title:"",
-                asker:"",
-            },
-            ans : {
-                questionId:0,
-                solver:store.state.username,
-                content:"",//this.form.content,
-            }
-        }
+      return {
+        form: {
+          title: "",
+          content: "",
+        },
+        imag: false,
+      };
     },
     methods: {
-        addAns() {
-            console.log(this.ans);
-            axios({
-                    method: 'POST',
-                    url: 'http://localhost:8080/questions/findByTitle',
-                    data: this.form.title,
-                    headers: {
-                        'Content-Type': 'application/json; charset=utf-8'
-                    }
-                    
-                })
-                    .then(response => {
-                        //console.log(response.data[0].id);
-                        this.ans.content=this.form.content;
-                        this.ans.questionId=response.data[0].id
-                        axios.post('http://localhost:8080/answers/addAns', this.ans)
-                        .then(response => {
-
-                        })
-                        .catch(error => {
-                        console.log(error);
-                        });
-                    })
-                    .catch(error => {
-                        console.log(error);
-                        //reject(error); // 将错误对象传递给 reject 函数
-                    });
-            //console.log(response)
-        },
+        async addText() {
+        await axios({
+          method: "POST",
+          url: "http://localhost:8080/api/image/create",
+          data: this.form.content,
+          headers: {
+            "Content-Type": "application/json; charset=utf-8",
+          },
+        })
+          .then((response) => {
+            console.log(this.form.content);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        this.imag = true;
+      },
+      toggleImage() {
+        this.imag = !this.imag;
+      },
+      discover() {
+        this.imag=false;
+      }
     },
-    mounted(){
-        this.form.title = this.$route.query.quest;
-    },
-    activated() {
-        this.form.title = this.$route.query.quest;
-    }
-}
-</script>
-<style scoped>
-    .el-card{
-        width: 100%;
-    }
-</style>
+  };
+  </script>
+  
+  <style scoped>
+  .el-card {
+    width: 100%;
+  }
+  </style>
+  
